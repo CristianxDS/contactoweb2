1420cf6af2ec410250069a3d4c47eb4c138ae908
Cristian.Sabando <cristianxdpk@gmail.com>
2025-02-23 18:38:07 -0500

    arreglos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Agrega CommonModule aquí
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  // Lista de imágenes
+  images: string[] = [
+    'assets/image/1.jpg',
+    'assets/image/2.jpg',
+    'assets/image/3.jpg',
+    'assets/image/4.jpg',
+    'assets/image/5.jpg',
+    'assets/image/6.jpg',
+    'assets/image/7.jpg',
+    'assets/image/8.jpg'
+  ];
 
+  // Índice de la imagen seleccionada
+  selectedIndex: number | null = null;
+
+  // Método para seleccionar una imagen
+  selectImage(index: number): void {
+    this.selectedIndex = index;
+  }
 }
